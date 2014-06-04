becdb133d0ac473c2bbdd399f16fc3d1152901f0
Maurice de Beijer <mauricedb@computer.org>
2014-06-04 19:21:14 +0200

    Added ReadOnlyApiController<T>

---
+++ AngularJS.ASPNET.Helpers.Specs/WebApi/ReadOnlyApiControllerOfModel.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http.Results;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TheProblemSolver.ASPNET.Helpers.WebApi;
+
+namespace AngularJS.ASPNET.Helpers.Specs.WebApi
+{
+    [TestClass]
+    public class ReadOnlyApiControllerOfModel
+    {
+        [TestMethod]
+        public void GetShouldReturnAllModels()
+        {
+            // Arrange
+            var controller = new ObjectApiController();
+
+            // Act
+            IEnumerable<Model> result = controller.Get();
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Count().Should().Be(2);
+        }
+
+        [TestMethod]
+        public void GetOneShouldReturnTheModel()
+        {
+            // Arrange
+            var controller = new ObjectApiController();
+
+            // Act
+            var result = controller.Get(1) as OkNegotiatedContentResult<Model>;
+
+            // Assert
+            result.Should().NotBeNull();
+            //result.Content.Should().NotBeNull();
+            result.Content.ShouldBeEquivalentTo(new Model {Id = 1});
+        }
+
+        [TestMethod]
+        public void GetThreeShouldReturnNotFound()
+        {
+            // Arrange
+            var controller = new ObjectApiController();
+
+            // Act
+            var result = controller.Get(3) as NotFoundResult;
+
+            // Assert
+            result.Should().NotBeNull();
+        }
+
+        private class Model
+        {
+            public int Id { get; set; }
+        }
+
+        private class ObjectApiController : ReadOnlyApiController<Model>
+        {
+            private readonly List<Model> _list;
+
+            public ObjectApiController()
+            {
+                _list = new List<Model>
+                {
+                    new Model {Id = 1},
+                    new Model {Id = 2},
+                };
+            }
+
+            protected override IEnumerable<Model> GetAll()
+            {
+                return _list;
+            }
+
+            protected override Model GetSingle(int id)
+            {
+                return _list.SingleOrDefault(m => m.Id == id);
+            }
+        }
+    }
+}
---
+++ AngularJS.ASPNET.Helpers/WebApi/ReadOnlyApiController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace TheProblemSolver.ASPNET.Helpers.WebApi
+{
+    public abstract class ReadOnlyApiController<T> : ApiController where T : class
+    {
+        public IEnumerable<T> Get()
+        {
+            return GetAll();
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            T result = GetSingle(id);
+            if (result == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(result);
+        }
+
+        protected abstract IEnumerable<T> GetAll();
+        protected abstract T GetSingle(int id);
+    }
+}
